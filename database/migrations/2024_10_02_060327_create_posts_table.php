e818f873b6aea19b9496b8def4d8117076b4f1f4
Md.Rashedul Islam <49880340+MKRashed@users.noreply.github.com>
2024-10-09 21:59:46 +0600

    Update 2024_10_02_060327_create_posts_table.php
    mirgration modify

--- database/migrations/2024_10_02_060327_create_posts_table.php
+++ database/migrations/2024_10_02_060327_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('auth_id');
             $table->longText('body');
+            $table->text('image');
             $table->timestamps();
         });
     }
